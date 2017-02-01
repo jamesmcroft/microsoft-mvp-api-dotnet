68b6b16608b426d72ac5080e04508e9dcf2503e4
James Croft <jamz_c@hotmail.co.uk>
2017-02-01 22:32:31 +0000

    Added logout logic and writing code exchange for MSA authentication.

--- MVP.Api/MVP.Api/ApiClient.cs
+++ MVP.Api/MVP.Api/ApiClient.cs
@@ -3,11 +3,15 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net.Http;
     using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     using MVP.Api.Models.MicrosoftAccount;
 
     using WinUX;
+    using WinUX.Networking.Requests.Json;
 
     /// <summary>
     /// Defines a mechanism to call into the MVP API from a client application.
@@ -58,6 +62,12 @@
         /// </summary>
         public string SubscriptionKey { get; }
 
+
+        /// <summary>
+        /// Gets the credentials associated with the logged in Microsoft account.
+        /// </summary>
+        public MSACredentials Credentials { get; private set; }
+
         /// <summary>
         /// Constructs a URL for authentication with Microsoft services and MVP API.
         /// </summary>
@@ -102,6 +112,68 @@
             uri.Query = query.ToString();
 
             return uri.Uri.ToString();
+        }
+
+        public Task<MSACredentials> ExchangeCodeAsync(
+            string code,
+            bool isTokenRefresh = false,
+            CancellationTokenSource cts = null)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                throw new ArgumentNullException(nameof(code), "The code for exchange cannot be null or empty.");
+            }
+
+            return Task.FromResult(default(MSACredentials));
+        }
+
+        /// <summary>
+        /// Logs out the user from the Microsoft services asynchronously.
+        /// </summary>
+        /// <returns>
+        /// Returns a value indicating whether the logout was successful.
+        /// </returns>
+        public async Task<bool> LogOutAsync()
+        {
+            var logoutSuccess = false;
+            try
+            {
+                var uri =
+                    $"{LogOutUri}?redirect_uri={Uri.EscapeUriString(RedirectUri)}&client_id={Uri.EscapeUriString(this.ClientId)}";
+
+                var client = new HttpClient();
+                var response = await client.GetAsync(uri);
+                logoutSuccess = response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+#endif
+            }
+            finally
+            {
+                this.Credentials = null;
+            }
+
+            return logoutSuccess;
+        }
+
+        private Dictionary<string, string> GetRequestHeaders()
+        {
+            if (!string.IsNullOrWhiteSpace(this.Credentials?.AccessToken))
+            {
+                var headers = new Dictionary<string, string>
+                                  {
+                                      {
+                                          "Authorization",
+                                          $"Bearer {this.Credentials.AccessToken}"
+                                      }
+                                  };
+                return headers;
+            }
+
+            return new Dictionary<string, string>();
         }
     }
 }
