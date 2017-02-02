34ee14e83c77435253181170a1476d4f1247f883
James Croft <jamz_c@hotmail.co.uk>
2017-02-02 17:05:15 +0000

    Completed profile API endpoint, adding online identity.

---
+++ MVP.Api/MVP.Api/ApiClient.OnlineIdentity.cs
@@ -0,0 +1,40 @@
+﻿namespace MVP.Api
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using MVP.Api.Models;
+
+    public partial class ApiClient
+    {
+        private const string OnlineIdentityEndpoint = "onlineidentities";
+
+        public async Task<IEnumerable<OnlineIdentity>> GetOnlineIdentitiesAsync(CancellationTokenSource cts = null)
+        {
+            return await this.GetAsync<IEnumerable<OnlineIdentity>>(OnlineIdentityEndpoint, true, null, cts);
+        }
+
+        public async Task<IEnumerable<OnlineIdentity>> GetOnlineIdentitiesByNominationsIdAsync(
+            Guid nominationsId,
+            CancellationTokenSource cts = null)
+        {
+            return await this.GetAsync<IEnumerable<OnlineIdentity>>(
+                       $"{OnlineIdentityEndpoint}/{nominationsId}",
+                       true,
+                       null,
+                       cts);
+        }
+
+        public async Task<OnlineIdentity> GetOnlineIdentityByIdAsync(int id, CancellationTokenSource cts = null)
+        {
+            return await this.GetAsync<OnlineIdentity>($"{OnlineIdentityEndpoint}/{id}", true, null, cts);
+        }
+
+        public async Task<bool> DeleteOnlineIdentityAsync(int id, CancellationTokenSource cts = null)
+        {
+            return await this.DeleteAsync<bool>($"{OnlineIdentityEndpoint}?id={id}", true, null, cts);
+        }
+    }
+}
---
+++ MVP.Api/MVP.Api/ApiClient.Profile.cs
@@ -0,0 +1,57 @@
+﻿namespace MVP.Api
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using MVP.Api.Models;
+
+    public partial class ApiClient
+    {
+        private const string ProfileEndpoint = "profile";
+
+        /// <summary>
+        /// Gets the MVP profile for the authenticated user asynchronously.
+        /// </summary>
+        /// <param name="cts">
+        /// Optional, a cancellation token source.
+        /// </param>
+        /// <returns>
+        /// Returns the MVP profile.
+        /// </returns>
+        public async Task<MVPProfile> GetMyProfileAsync(CancellationTokenSource cts = null)
+        {
+            return await this.GetAsync<MVPProfile>(ProfileEndpoint, true, null, cts);
+        }
+
+        /// <summary>
+        /// Gets the MVP profile image for the authenticated user as a base64 string asynchronously.
+        /// </summary>
+        /// <param name="cts">
+        /// Optional, a cancellation token source.
+        /// </param>
+        /// <returns>
+        /// Returns an image as a base64 string.
+        /// </returns>
+        public async Task<string> GetMyProfileImageAsync(CancellationTokenSource cts = null)
+        {
+            return await this.GetAsync<string>($"{ProfileEndpoint}/photo", true, null, cts);
+        }
+
+        /// <summary>
+        /// Gets an MVP profile for another MVP by their MVP ID asynchronously.
+        /// </summary>
+        /// <param name="mvpId">
+        /// The MVP ID, e.g. 5001534.
+        /// </param>
+        /// <param name="cts">
+        /// Optional, a cancellation token source.
+        /// </param>
+        /// <returns>
+        /// Returns the MVP profile.
+        /// </returns>
+        public async Task<MVPProfile> GetProfileAsync(string mvpId, CancellationTokenSource cts = null)
+        {
+            return await this.GetAsync<MVPProfile>($"{ProfileEndpoint}/{mvpId}", true, null, cts);
+        }
+    }
+}
